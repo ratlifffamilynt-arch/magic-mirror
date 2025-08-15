9cdba3ce84f85c4939a31278e5f537d035631e7a
ratlifffamilynt-arch <ratlifffamilynt@gmail.com>
2025-08-14 20:52:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var select = document.querySelector('select')
 function changeEffect() {
   video.style.filter = select.value
 }
+select.addEveentListerner('change', changeEffect)
 
 navigator.mediaDevices
   .getUserMedia({
