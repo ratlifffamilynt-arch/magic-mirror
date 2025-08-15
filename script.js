4d6719b29874aafdb3b7d4e84eb1ac236379c1c8
ratlifffamilynt-arch <ratlifffamilynt@gmail.com>
2025-08-15 17:20:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,19 @@ navigator.mediaDevices
 
 /*
   
-<h1>Magic Mirror</h1>
-<p>Choose a special effect</p>
-<video autoplay></video>
-<script src = "script.js"></script>
-  
+var video = document.querySelector('video'); 
+var select = document.querySelector('select')
+
+function changeEffect() {
+  video.style.filter = select.value
+}
+select.addEventListener('change', changeEffect);
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+  })
+  .then(function (stream) {
+    video.srcObject = stream; 
+  });  
 */
